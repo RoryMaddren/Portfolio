7cb16f081fcc6863f77a96c11edc68d031e48182
rorym <roryrmaddren@gmail.com>
2026-05-23 17:56:18 +1200

    locking nav bar scroller + works page selector now functional

--- js/generic.js
+++ js/generic.js
@@ -4,4 +4,5 @@ const offScreenMenu = document.querySelector('.ham-menu');
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
+  body.classList.toggle('lock-scroll');
 });
---
+++ js/work-overview.js
@@ -0,0 +1,27 @@
+  const uniBtn = document.querySelector('#uni-btn');
+  const industryBtn = document.querySelector('#industry-btn');
+
+  const uniWorks = document.querySelector('#uni-works');
+  const industryWorks = document.querySelector('#work-section-industry');
+
+  // Show Uni Work
+  uniBtn.addEventListener('click', () => {
+    uniWorks.classList.add('active');
+    industryWorks.classList.remove('active');
+
+    uniBtn.classList.add('active');
+    industryBtn.classList.remove('active');
+        console.log("working-1");
+
+  });
+  
+
+  // Show Industry Work
+  industryBtn.addEventListener('click', () => {
+    industryWorks.classList.add('active');
+    uniWorks.classList.remove('active');
+
+    industryBtn.classList.add('active');
+    uniBtn.classList.remove('active');
+        console.log("working-2");
+  });
