b044a67bbf7b9dc69a6c6cd96955660233ec5d33
rorym <roryrmaddren@gmail.com>
2026-05-23 18:01:27 +1200

    updating locking bug for nav

--- js/generic.js
+++ js/generic.js
@@ -1,8 +1,11 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const offScreenMenu = document.querySelector('.ham-menu');
+const body = document.body;
 
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
-  body.classList.toggle('lock-scroll');
+    body.classList.toggle('lock-scroll');
+  
 });
+
